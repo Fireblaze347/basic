7c7567316a01c26464741ed0ea3eec8458e9b63b
Fireblaze347 <theanonymousbeaver123@gmail.com>
2025-10-22 17:36:54 +0800

    hangman

---
+++ hangman.py
@@ -0,0 +1,128 @@
+import random
+import sys
+import os
+
+
+word_list = ["hangman", "banana"] #list of words that can be asked
+selected_word = random.choice(word_list) #the selected word (chosen randomly)
+known_letter_indexes  = [2, 6]
+letter_counter = 0
+current_word = []
+wrong_guessed_words = []
+mistakes = 0
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+#FYI they are allowed 6 mistakes
+for letter in selected_word:
+    ###cu
+    if letter_counter in known_letter_indexes:
+        current_word.append(letter)
+        
+    else:
+        current_word.append("_")
+    letter_counter += 1
+
+
+#Game loop
+while True:
+
+    os.system("clear")
+    print(HANGMANPICS[mistakes])
+    if wrong_guessed_words:
+        print(f"You have guessed {", ".join(wrong_guessed_words)} wrongly already")
+    print()
+    print(" ".join(current_word))
+    print()
+    user_letter = input("Guess a letter... ")
+
+    
+
+
+    #if they found the word early and guess it fully
+    if user_letter == selected_word:
+        break
+
+
+    if user_letter in selected_word:
+        #Check for multiple entries of a letter! IMPORTANT!
+
+        user_letter_index = [i for i, x in enumerate(selected_word) if x == user_letter] #A LIST CONTAINING INDEXES (ints)
+
+        #TRUE if more than one item
+        if len(user_letter_index) > 1:
+            #iterate over every extra one found
+            for specific_letter in user_letter_index:
+                current_word[specific_letter] = selected_word[specific_letter]
+        else:
+            #only one so we just take the first(only) one
+            #userletterindex[0] returns an int with the index!
+            current_word[user_letter_index[0]] = selected_word[user_letter_index[0]]
+
+
+        
+    else:
+        #the user is wrong 
+        wrong_guessed_words.append(user_letter)
+     
+        print("Letter not present! ")
+        print()
+        mistakes += 1
+    
+    if "".join(current_word) == selected_word:
+        #win
+        break
+
+    if len(wrong_guessed_words) > 6:
+        print("You lost!")
+        sys.exit()
+
+print("You win!")
