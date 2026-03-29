fd387caba533b828ed4f2ca2fb1509fcade46cd3
Fireblaze347 <153418829+Fireblaze347@users.noreply.github.com>
2026-03-29 13:21:01 +0800

    some stuff

--- pycharm_updated/hangman.py
+++ pycharm_updated/hangman.py
@@ -1,4 +1,4 @@
-import sys, os, re, random
+import sys, os, re, random, json
 
 def clear():
     os.system("cls")
@@ -6,32 +6,24 @@ def clear():
 def find_topic_choice():
     return input(" [1] Animals\n [2] Countries\n [3] Food \n [4] Elements\n")
 
-
-clear()
-
-topic_choice = find_topic_choice()
-
-match topic_choice:
-    case "1":
-        pass
+def read_json(file_name):
+    with open(file_name, 'r') as json_file:
+        json_data = json.load(json_file)
         
+    return json_data
 
-selected_word = "united states of america"  # the selected word (chosen randomly)
-known_letter_indexes = [2, 6]
-letter_counter = 0
-current_word = []
-wrong_guessed_words = []
-mistakes = 0
 
-hangman_pictures = ['''
-   +------+
+def main():
+    print(read_json('word_list.json')["1"])
+    hangman_pictures = ['''
+   --------
    |      |
 	  |
 	  |
 	  |
 	  |
 	  |''', '''
-   +------+
+   --------
    |      |
    O	  |
 	  |
@@ -39,7 +31,7 @@ hangman_pictures = ['''
 	  |
 	  |
 ''', '''
-   +------+
+   --------
    |      |
    O	  |
    |	  |
@@ -47,7 +39,7 @@ hangman_pictures = ['''
 	  |
 	  |
 ''', '''
-   +------+
+   --------
    |      |
    O	  |
   /|	  |
@@ -55,7 +47,7 @@ hangman_pictures = ['''
 	  |
 	  |
 ''', '''
-   +------+
+   --------
    |      |
    O	  |
   /|\	  |
@@ -63,7 +55,7 @@ hangman_pictures = ['''
 	  |
 
 ''', '''
-   +------+
+   --------
    |      |
    O	  |
   /|\	  |
@@ -71,87 +63,113 @@ hangman_pictures = ['''
   /	  |
 	  |
 ''', '''
-   +------+
+   --------
    |      |
    O	  |
   /|\	  |
    |	  |
   / \	  |
 	  |''']
-
-
-clear()
-
-# FYI they are allowed 6 mistakes
-for letter in selected_word:
-    ###cu
-    if letter_counter in known_letter_indexes or letter == " ":
-        current_word.append(letter)
     
-    else:
-        current_word.append("_")
-    letter_counter += 1
+    
+    clear()
 
-# Game loop
-while True:
+    topic_choice = find_topic_choice()
+
+    match topic_choice:
+        case "1":
+            pass
+            
+
+    selected_word = "united states of america"  # the selected word (chosen randomly)
+    known_letter_indexes = [2, 6]
+    letter_counter = 0
+    current_word = []
+    wrong_guessed_words = []
+    mistakes = 0
+
+
+    clear()
+
+    # FYI they are allowed 6 mistakes
+    for letter in selected_word:
+        ###cu
+        if letter_counter in known_letter_indexes or letter == " ":
+            current_word.append(letter)
+        
+        else:
+            current_word.append("_")
+        letter_counter += 1
+
+    # Game loop
+    while True:
+        print(hangman_pictures[mistakes])
+        if wrong_guessed_words:
+            print(f"You have guessed {", ".join(wrong_guessed_words)} wrongly already")
+        print()
+        print(" ".join(current_word))
+        print()
+        
+        user_letter = input("Guess a letter... ")
+
+        if re.search(r"[a-z]", user_letter):
+            # if they found the word early and guess it fully
+            if user_letter == selected_word:
+                break
+
+            if user_letter in selected_word:
+                # Check for multiple entries of a letter! IMPORTANT!
+
+                #https://stackoverflow.com/questions/6294179/how-to-find-all-occurrences-of-an-element-in-a-list
+                user_letter_index = [i for i, x in enumerate(selected_word) if x == user_letter]  # A LIST CONTAINING INDEXES (ints)
+
+
+                # TRUE if more than one item
+                if len(user_letter_index) > 1:
+                    # iterate over every extra one found
+                    for specific_letter in user_letter_index:
+                        current_word[specific_letter] = selected_word[specific_letter]
+                elif not len(user_letter_index) == 0:            
+                    # only one so we just take the first(only) one
+                    # userletterindex[0] returns an int with the index!
+                    current_word[user_letter_index[0]] = selected_word[user_letter_index[0]]
+
+            elif user_letter not in wrong_guessed_words:
+                # the user is wrong
+                wrong_guessed_words.append(user_letter)
+
+                print("Letter not present! ")
+                print()
+                mistakes += 1
+
+            if "".join(current_word) == selected_word:
+                # win
+                break
+
+            if len(wrong_guessed_words) > 6:
+                print("You lost!")
+                print()
+                print(f"The word was: {selected_word}\n")
+                sys.exit()
+
+            clear()
+        else:
+            # not a letter
+            clear()
+            
+    clear()
     print(hangman_pictures[mistakes])
     if wrong_guessed_words:
         print(f"You have guessed {", ".join(wrong_guessed_words)} wrongly already")
     print()
     print(" ".join(current_word))
     print()
-    
-    user_letter = input("Guess a letter... ")
-
-    if re.search(r"[a-z]", user_letter):
-        # if they found the word early and guess it fully
-        if user_letter == selected_word:
-            break
-
-        if user_letter in selected_word:
-            # Check for multiple entries of a letter! IMPORTANT!
-
-            user_letter_index = [i for i, x in enumerate(selected_word) if x == user_letter]  # A LIST CONTAINING INDEXES (ints)
+    print("You win!\n")
 
 
-            # TRUE if more than one item
-            if len(user_letter_index) > 1:
-                # iterate over every extra one found
-                for specific_letter in user_letter_index:
-                    current_word[specific_letter] = selected_word[specific_letter]
-            elif not len(user_letter_index) == 0:            
-                # only one so we just take the first(only) one
-                # userletterindex[0] returns an int with the index!
-                current_word[user_letter_index[0]] = selected_word[user_letter_index[0]]
 
-        elif user_letter not in wrong_guessed_words:
-            # the user is wrong
-            wrong_guessed_words.append(user_letter)
+if __name__ == "__main__":
+    main()
 
-            print("Letter not present! ")
-            print()
-            mistakes += 1
 
-        if "".join(current_word) == selected_word:
-            # win
-            break
 
-        if len(wrong_guessed_words) > 6:
-            print("You lost!")
-            print()
-            print(f"The word was: {selected_word}")
-            sys.exit()
-
-        clear()
-    else:
-        # not a letter
-        clear()
-        
-clear()
-print(hangman_pictures[mistakes])
-if wrong_guessed_words:
-    print(f"You have guessed {", ".join(wrong_guessed_words)} wrongly already")
-print()
-print(" ".join(current_word))
-print()
-print("You win!\n")
