77670d8fce3bdac03670b86ca341a7bc597e06c8
Fireblaze347 <153418829+Fireblaze347@users.noreply.github.com>
2026-03-29 11:57:40 +0800

    progress

--- pycharm_updated/script.py
+++ pycharm_updated/script.py
@@ -1,6 +1,13 @@
-selected_word = "buffoon"
-user_letter = "oo"
+selected_word = "snow leopard"
+user_letter = "snow"
+M_selected_word = []
 
 user_letter_index = [i for i, x in enumerate(selected_word) if x == user_letter]  # A LIST CONTAINING INDEXES (ints)
+
+for i in range(len(selected_word)):
+    for n in user_letter:
+        if n == selected_word[i]:
+            user_letter_index.append(i)
+            break
 
 print(user_letter_index)
